ba917f87bb89cfccc6ca2a8b789072d049147c8c
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2024-09-02 23:16:59 +0300

    Add files via upload

--- 4/script.js
+++ 4/script.js
@@ -2,5 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const checkoutButton = document.querySelector('.checkout-button');
     checkoutButton.addEventListener('click', function() {
         alert('Checkout button clicked!');
+		window.location.href = '../5/index.html';
     });
 });
