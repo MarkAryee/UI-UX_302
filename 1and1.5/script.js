ba348bfcc4dd9d6931717396ce566fd4e717597f
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2024-09-13 18:42:19 +0300

    Add files via upload

---
+++ 1and1.5/script.js
@@ -0,0 +1,31 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.querySelector('.login-button');
+    loginButton.addEventListener('click', function() {
+        window.location.href = '../2/index.html';;
+    });
+});	
+
+//SIGN UP BUTTON
+function slideOutAndRedirect2() {
+            document.body.style.animation = 'slideOut 1s forwards';
+            setTimeout(function() {
+                window.location.href = '2.html'; // Replace with your next page's filename
+            }, 100); // Wait for the slide out animation to complete
+        }	
+		
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.querySelector('.signup-button');
+    loginButton.addEventListener('click', function() {
+        setTimeout(slideOutAndRedirect2, 10);
+    });
+});		
+	
+		
+	
+		
+		
+		
+		
+		
+		
